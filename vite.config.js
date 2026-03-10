df7fe7d9c4b33d6b793986812fa817bb8247ceee
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-03-10 17:09:03 -0400

    fix simplelightbox import

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/goit-js-hw-09/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gallery: resolve(__dirname, '1-gallery.html'),
+        form: resolve(__dirname, '2-form.html'),
+      },
+    },
+  },
 });
