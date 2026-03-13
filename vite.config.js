c062bc746d93725e68ac45b0adc95fec2b09ca28
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-03-13 14:47:19 -0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/goit-js-hw-09/',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        gallery: resolve(__dirname, '1-gallery.html'),
-        form: resolve(__dirname, '2-form.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        gallery: resolve(__dirname, 'src/1-gallery.html'),
+        form: resolve(__dirname, 'src/2-form.html'),
       },
     },
   },
-});
+})
