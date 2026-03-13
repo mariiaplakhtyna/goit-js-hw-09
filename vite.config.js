22305b561fb490046ad20cd13757c42495dfa6bb
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-03-13 14:51:53 -0400

    fix html paths

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/goit-js-hw-09/',
@@ -12,4 +12,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
