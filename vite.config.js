98aade49b01cd61b9eece8c200431bedd4905513
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-03-27 11:37:26 -0400

    fix vite root

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: 'src',
   base: '/goit-js-hw-09/',
 
   build: {
+    outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
